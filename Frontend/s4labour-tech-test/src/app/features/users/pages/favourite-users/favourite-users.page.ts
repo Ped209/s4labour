6eca210238bdd54623842e3a1a5f2f22d42f33e1
Tom Pedley <tompedley@gmail.com>
2025-11-21 11:11:45 +0000

    feat: favourites component implementation

--- Frontend/s4labour-tech-test/src/app/features/users/pages/favourite-users/favourite-users.page.ts
+++ Frontend/s4labour-tech-test/src/app/features/users/pages/favourite-users/favourite-users.page.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { UsersList } from '@users/components';
+import { UsersService } from '@users/services';
 
 @Component({
   selector: 'app-favourite-users',
-  imports: [],
+  imports: [MatCardModule, MatIconModule, UsersList, RouterLink],
   templateUrl: './favourite-users.page.html',
   styleUrl: './favourite-users.page.scss',
 })
 export class FavouriteUsersPage {
-
+  usersService = inject(UsersService);
+  
+  favouriteUsers = computed(() => 
+    this.usersService.users().filter(u => u.isFavourite)
+  );
 }
